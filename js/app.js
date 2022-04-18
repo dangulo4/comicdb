1b85d213b1afde5ebfee523b7b3307d281d0ccd7
Daniel Angulo <dangulo4@gmail.com>
2022-04-17 18:47:15 -0700

    added paragraph and credit to options

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//  Everything in this file is Javascript
+console.log('hello world!');
+
+// local storage save
+localStorage.setItem('title', 'Star Wars');
+localStorage.setItem('number', '1');
+localStorage.setItem('year', '1977');
+localStorage.setItem('publisher', 'Marvel Comics Group');
+localStorage.setItem('notes', 'Printed by Marvel Comics in 1977');
